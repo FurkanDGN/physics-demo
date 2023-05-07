5a749cc3089053ec73d837525447cf863ea9400d
Furkan Doğan <furkanaxx34@gmail.com>
2023-05-07 16:54:53 +0300

    Refactor

--- physics-common/src/main/java/me/furkandgn/physicsdemo/common/collision/Collision.java
+++
@@ -1,7 +0,0 @@
-package me.furkandgn.physicsdemo.common.collision;
-
-import me.furkandgn.physicsdemo.common.Body;
-import org.joml.Vector2d;
-
-public record Collision(Body bodyA, Body bodyB, Vector2d normal, double penetrationDepth) {
-}
